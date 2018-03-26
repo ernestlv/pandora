10d42a2b731bffa94968665a6d94bb437b3ae717
ernestlv <ernestolv@hotmail.com>
2018-03-26 09:53:11 -0400

    small fix pie chart code

--- public/js/pieChart.js
+++ public/js/pieChart.js
@@ -90,7 +90,7 @@ class PieChart extends React.Component {
 	pickColor(){
 		const color = ["#2484c1","#0c6197","#4daa4b","#90c469","#daca61","#e4a14b","#e98125","#cb2121","#830909","#923e99","#ae83d5","#bf273e","#ce2aeb","#bca44a","#618d1b","#1ee67b","#b0ec44","#a4a0c9","#322849","#86f71a","#d1c87f","#7d9058","#44b9b0","#7c37c0","#cc9fb1","#e65414","#8b6834","#248838"];
 		this.currentColor++;
-		return color[this.currentColor%28];
+		return color[this.currentColor%color.length];
 	}
 
 	formatEvents(events){
